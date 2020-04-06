572138228048edba106e6a8fa1f496d9123e0ae6
jaredmalkin <35544560+jaredmalkin@users.noreply.github.com>
2020-04-06 02:02:26 -0500

    Update Player.java

--- src/Player.java
+++ src/Player.java
@@ -7,6 +7,8 @@ public class Player {
 //////////
 private double posX;
 private double posY; 
+public double velocityX;
+public double velocityY;
 private final double startPosX;
 private final double startPosY;
 private final double width = 0.1;
@@ -35,47 +37,61 @@ StdDraw.filledRectangle(this.posX, this.posY, this.width/2, this.height/2);
  */
 public void move() {
 
-//If movements are possible:
-if (player == 0){
-if ((ArcadeKeys.isKeyPressed(0, 1)) && this.posX > -1) {
-this.posX -= 0.05;
-}
-else if ((ArcadeKeys.isKeyPressed(0, 3)) && this.posX < -0.1) {
-this.posX += 0.05;
-}
-else if ((ArcadeKeys.isKeyPressed(0, 0)) && this.posY < 1) {
-double hold = 0;
-hold = this.posY + .25;
+	//player movement with constraints of the boundaries of the field
+	if (player == 0){
+		if ((ArcadeKeys.isKeyPressed(0, 1)) && this.posX>-1) {
+			//key pressed changes velocity
+			this.velocityX -= 0.005;
+		}
+		else if ((ArcadeKeys.isKeyPressed(0, 3)) && this.posX<-0.1) {
+			this.velocityX += 0.005;
+		}
+		else if ((ArcadeKeys.isKeyPressed(0, 0)) && this.posY <= -0.95) {
+			//a jump starts you off at a fixed velocity, if you are currently on the ground
+			this.velocityY = 0.1;
 
-if(posY < hold) {
-this.posY += 0.05;
-}
-}
-}
-if (player == 1){
-if ((ArcadeKeys.isKeyPressed(1, 1)) && this.posX > 0.1) {
-this.posX -= 0.05;
-}
-else if ((ArcadeKeys.isKeyPressed(1, 3)) && this.posX < 1 ) { //Y coordinate: Up is 0;
-this.posX += 0.05;
-}
-else if ((ArcadeKeys.isKeyPressed(1, 0)) && this.posY < 1) {
-this.posY += 0.05;
-}
-}
+		}
+	}
+	if (player == 1){
+		if ((ArcadeKeys.isKeyPressed(1, 1)) && this.posX > 0.1) {
+			//key pressed changes velocity
+			this.velocityX -= 0.005;
+		}
+		else if ((ArcadeKeys.isKeyPressed(1, 3)) && this.posX < 1) {
+			this.velocityX += 0.005;
+		}
+		else if ((ArcadeKeys.isKeyPressed(1, 0)) && this.posY <= -0.95) {
+			//a jump starts you off at a fixed velocity, if you are currently on the ground
+			this.velocityY = 0.1;
+		}
+	}
+	
+	//velocities update the position
+	this.posX += this.velocityX;
+	this.posY += this.velocityY;
+	
+	//vertical acceleration updates the velocity
+	if(this.posY > -0.95) {
+		this.velocityY = this.velocityY - 0.0000005;
+	}
+	
+	this.velocityX = 0;
+	
+	
+	
 }
 
 
 
 public double getPosX() {
-return this.posX;
+	return this.posX;
 }
 
 public double getPosY() {
-return this.posY;
+	return this.posY;
 }
 public void gravity() {
-this.posY = this.posY - 0.01;
+	this.velocityY = this.velocityY - 0.005;
 }
 
 
