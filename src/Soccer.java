5349eb9e570c8aa9c7c84cbdb703b3d64531b2b0
Mustapha <barriem@wustl.edu>
2020-04-05 21:34:14 -0500

    set background

--- src/Soccer.java
+++ src/Soccer.java
@@ -1,12 +1,14 @@
 import java.util.*;
+
+import com.sun.prism.paint.Color;
+
 import sedgewick.StdDraw;
 public class Soccer {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("100");
-		StdDraw.filledCircle(512,512 ,123);
-		
+		StdDraw.setCanvasSize(1364,768);
+		StdDraw.picture(0.5,0.5,"background.png");	
 	}
 
 }
