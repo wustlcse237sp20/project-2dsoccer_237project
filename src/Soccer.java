ea5e5f88abec542af3e050f50a9d0b1c6e158812
Amin Bdeir <aminbdeir@wustl.edu>
2020-04-05 19:51:39 -0500

    library changes

--- src/Soccer.java
+++ src/Soccer.java
@@ -1,9 +1,12 @@
-
+import java.util.*;
+import sedgewick.StdDraw;
 public class Soccer {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("100");
+		StdDraw.filledCircle(512,512 ,123);
+		
 	}
 
 }
