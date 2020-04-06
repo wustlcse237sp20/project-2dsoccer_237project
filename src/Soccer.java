be605357d480ecb14a761d370a539f5e3da0e185
Mustapha <barriem@wustl.edu>
2020-04-05 22:14:17 -0500

    merge conflict

--- src/Soccer.java
+++ src/Soccer.java
@@ -6,7 +6,6 @@ import sedgewick.StdDraw;
 public class Soccer {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		StdDraw.setCanvasSize(1364,768);
 		StdDraw.picture(0.5,0.5,"background.png");	
 	}
