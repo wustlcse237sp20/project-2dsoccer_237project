dc0f02b04a2af8775a49f322088387e527ac55c9
Amin Bdeir <aminbdeir@wustl.edu>
2020-04-05 21:14:11 -0500

    added arcadekeys

--- src/Soccer.java
+++ src/Soccer.java
@@ -1,11 +1,13 @@
 import java.util.*;
 import sedgewick.StdDraw;
+import java.awt.Color;
 public class Soccer {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("100");
-		StdDraw.filledCircle(512,512 ,123);
+		StdDraw.setPenColor(Color.RED);
+		StdDraw.filledRectangle(.25, .5, .25, .5);
 		
 	}
 
