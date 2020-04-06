2b15d8f91c284ce5e0874324b085c64364f8a8d4
DongJeff <jiefeidong@wustl.edu>
2020-04-05 22:12:18 -0500

    main screen

--- src/Soccer.java
+++ src/Soccer.java
@@ -6,7 +6,19 @@ public class Soccer {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 				StdDraw.setCanvasSize(1364,768);
-				StdDraw.picture(0.5,0.5,"background.png");	
+				StdDraw.setPenColor(Color.black);
+				StdDraw.filledRectangle(0.5, 0.5, 682, 384);
+				StdDraw.setPenColor(Color.white);
+				StdDraw.text(0.5, 0.8, "2-D Soccer");
+				StdDraw.text(0.5, 0.4, "Right Click to Continue");
+				StdDraw.show(0);
+				StdDraw.picture(0.5,0.5,"background.png");
+				while(true) {
+					if(StdDraw.mousePressed()) {
+						StdDraw.show(0);
+					}
+				}
+
 			}
 	}
 
