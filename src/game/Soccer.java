970f9c79f8f8b4458ab39560c0003463e2481755
DongJeff <jiefeidong@wustl.edu>
2020-05-03 15:26:22 -0500

    input names

--- src/game/Soccer.java
+++ src/game/Soccer.java
@@ -1,9 +1,14 @@
 package game;
-
 import java.util.*;
+<<<<<<< Updated upstream
+=======
+import java.util.concurrent.TimeUnit;
+>>>>>>> Stashed changes
 import sedgewick.StdDraw;
+import cse131.ArgsProcessor;
 import java.awt.Color;
 public class Soccer {
+<<<<<<< Updated upstream
 	private Player player1;
 	private Player player2;
 
@@ -20,6 +25,32 @@ public class Soccer {
 					board.movePlayers(player1,player2);
 					StdDraw.show(2);
 					board.handleJumping(player1, player2);
+=======
+	public Player player1;
+	public Player player2;
+
+
+	public static void main(String[] args) {
+		ArgsProcessor ap = new ArgsProcessor(args);
+		boolean decision = true;
+		while(decision == true) {
+			String player1Name = ap.nextString("Player 1 name?");
+			String player2Name = ap.nextString("Player 2 name?");
+			Board board = new Board();
+			board.setupScreen();
+			Player player1 = new Player(-1, -0.95, 0); //Not calling "Player" causes static/non static error
+			Player player2 = new Player(1, -0.95, 1);
+			Ball ball = new Ball(0,0.5);
+			int timer = 0;
+			int count = 0;
+			boolean isgameOver = false;
+			while(isgameOver == false) {
+				StdDraw.clear();
+				StdDraw.picture(0,0,"background.png");
+				isgameOver = runGame(board, player1, player2, ball, count);
+				if(timer % 100 == 0) {
+					count++;
+>>>>>>> Stashed changes
 				}
 	}
 }
